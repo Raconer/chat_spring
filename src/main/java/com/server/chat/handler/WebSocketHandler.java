7c884b29ba35ec0a0b63b3d8e09064323149b5ce
Raconer <cha11enge@hanmail.net>
2022-12-19 16:55:44 +0900

    Revert "기본 채팅 설정"
    This reverts commit 5c0e407b6dc6ad5302c7d960547d9a2b3aa3cab5.

--- src/main/java/com/server/chat/handler/WebSocketHandler.java
+++
@@ -1,9 +0,0 @@
-package com.server.chat.handler;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-@Component
-public class WebSocketHandler extends TextWebSocketHandler {
-
-}
